70cdd82ade5301789fb93649d04ab65b0fc7a4fa
mygoilegoy <anthony.nhu@outlook.fr>
2025-02-07 16:16:52 +0100

    add types

--- exercices-react/src/main.tsx
+++ exercices-react/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Accueil from './pages/accueil.tsx'
+import Accueil from './pages/index.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- exercices-react/src/pages/compteur.tsx
+++ exercices-react/src/pages/compteur.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './compteur.css'
+import './styles.css'
 
 function Compteur() {
   const [count, setCount] = useState(0)
